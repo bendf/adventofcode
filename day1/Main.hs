a4dfdbf48204a6a48804f2e22db31e101774b683
Benjamin Dudiak-Fry <bdudiakfry@gmail.com>
2019-12-01 18:35:44 +0000

    Day one, part two

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,12 +1,24 @@
+import Data.List
 
 type Mass = Integer
 type Fuel = Integer
 
-calcFuel :: Mass -> Fuel
-calcFuel m = (div m 3) - 2
 
-foo :: String -> String
-foo str =  show . sum $ calcFuel . read <$> words str
+calcFuel :: Mass -> Fuel
+calcFuel mass = (div mass 3) - 2
+
+totalFuel :: Mass -> Fuel
+totalFuel mass = sum . tail . takeWhile (>0) $ iterate calcFuel mass
+
+partOne :: String -> String
+partOne str =  show . sum $ calcFuel . read <$> words str
+
+partTwo :: String -> String
+partTwo str = show . sum $ totalFuel . read <$> words str
+
+result :: (String -> String) -> (String -> String) -> String -> String
+result f g str = intercalate "\n" [ "Part One: " ++ (f str), "Part Two: " ++ (g str)]
+
 
 main :: IO ()
-main = interact foo
+main = interact $ result partOne partTwo
